3cc21bd92eec7aa88ed0f952acfd775316d4d660
Aleksei V. Simonov <wert001@yandex.ru>
2025-12-15 15:22:54 +0300

    add dependencies for hilt and cameraX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.plugin)
+
 }
 
 android {
@@ -57,4 +60,16 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // CameraX
+
+    implementation(libs.camerax.core)
+    implementation(libs.camerax.camera2)
+    implementation(libs.camerax.lifecycle)
+    implementation(libs.camerax.view)
+
+    //hilt
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.android)
+
 }
